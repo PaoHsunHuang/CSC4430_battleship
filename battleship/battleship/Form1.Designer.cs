9500f32fdd56dc005207268fcba89e25abc50b30
Paohsun Huang <allen860706@gmail.com>
2018-10-11 18:40:09 -0700

    10/11 only outlook

---
+++ battleship/battleship/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace battleship
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.eventLog1 = new System.Diagnostics.EventLog();
+            this.LogText = new System.Windows.Forms.Label();
+            this.LogBox = new System.Windows.Forms.GroupBox();
+            this.RestartBtn = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Horizontal = new System.Windows.Forms.RadioButton();
+            this.Vertical = new System.Windows.Forms.RadioButton();
+            this.ExitBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).BeginInit();
+            this.LogBox.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // eventLog1
+            // 
+            this.eventLog1.SynchronizingObject = this;
+            // 
+            // LogText
+            // 
+            this.LogText.Location = new System.Drawing.Point(16, 18);
+            this.LogText.Name = "LogText";
+            this.LogText.Size = new System.Drawing.Size(374, 84);
+            this.LogText.TabIndex = 0;
+            this.LogText.Text = "123";
+            // 
+            // LogBox
+            // 
+            this.LogBox.Controls.Add(this.LogText);
+            this.LogBox.Location = new System.Drawing.Point(196, 356);
+            this.LogBox.Name = "LogBox";
+            this.LogBox.Size = new System.Drawing.Size(396, 114);
+            this.LogBox.TabIndex = 1;
+            this.LogBox.TabStop = false;
+            this.LogBox.Text = "Log";
+            // 
+            // RestartBtn
+            // 
+            this.RestartBtn.Location = new System.Drawing.Point(616, 365);
+            this.RestartBtn.Name = "RestartBtn";
+            this.RestartBtn.Size = new System.Drawing.Size(97, 93);
+            this.RestartBtn.TabIndex = 2;
+            this.RestartBtn.Text = "Restart";
+            this.RestartBtn.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Horizontal);
+            this.groupBox1.Controls.Add(this.Vertical);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(86, 86);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Direction";
+            // 
+            // Horizontal
+            // 
+            this.Horizontal.AutoSize = true;
+            this.Horizontal.Location = new System.Drawing.Point(6, 55);
+            this.Horizontal.Name = "Horizontal";
+            this.Horizontal.Size = new System.Drawing.Size(72, 16);
+            this.Horizontal.TabIndex = 1;
+            this.Horizontal.TabStop = true;
+            this.Horizontal.Text = "Horizontal";
+            this.Horizontal.UseVisualStyleBackColor = true;
+            // 
+            // Vertical
+            // 
+            this.Vertical.AutoSize = true;
+            this.Vertical.Checked = true;
+            this.Vertical.Location = new System.Drawing.Point(6, 21);
+            this.Vertical.Name = "Vertical";
+            this.Vertical.Size = new System.Drawing.Size(59, 16);
+            this.Vertical.TabIndex = 0;
+            this.Vertical.TabStop = true;
+            this.Vertical.Text = "Vertical";
+            this.Vertical.UseVisualStyleBackColor = true;
+            // 
+            // ExitBtn
+            // 
+            this.ExitBtn.Location = new System.Drawing.Point(719, 365);
+            this.ExitBtn.Name = "ExitBtn";
+            this.ExitBtn.Size = new System.Drawing.Size(97, 93);
+            this.ExitBtn.TabIndex = 4;
+            this.ExitBtn.Text = "Exit";
+            this.ExitBtn.UseVisualStyleBackColor = true;
+            this.ExitBtn.Click += new System.EventHandler(this.ExitBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(844, 511);
+            this.Controls.Add(this.ExitBtn);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.RestartBtn);
+            this.Controls.Add(this.LogBox);
+            this.Name = "Form1";
+            this.Text = "Battleship";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).EndInit();
+            this.LogBox.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Diagnostics.EventLog eventLog1;
+        private System.Windows.Forms.GroupBox LogBox;
+        private System.Windows.Forms.Label LogText;
+        private System.Windows.Forms.Button RestartBtn;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton Horizontal;
+        private System.Windows.Forms.RadioButton Vertical;
+        private System.Windows.Forms.Button ExitBtn;
+    }
+}
+
---
+++ battleship/battleship/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace battleship
+{
+    public partial class Form1 : Form
+    {
+        string[] ShipName = {"Aircraft","Battleship","Submarine",
+            "Destroyer","Patrol"};
+        int[] length = { 5, 4, 3, 3, 2 };
+        int PlayerNum = 2;
+        int returnValue;
+        struct location
+        {
+            public int x_axis;
+            public int y_axis;
+
+        }
+        //set the struct for the ships
+        //include information about ships
+        //location, name ,direction and remain
+        //direction 0 = horizontal, 1 = vertical
+        struct ship
+        {
+            public int number;
+            public string name;
+            public location location;
+            public int direction;
+            public int remain;
+        }
+
+        //sturct for player
+        //include 2 boards for each players
+        //board will be 3D array
+        //first variable show which board
+        //first board will have their own ships
+        //second board will show the hit or miss
+        //second and third board show x,y axis
+        //TotalRemain show how many ship player left
+        struct player
+        {
+            public char[,,] board;
+            public ship[] AllShip;
+            public int TotalRemain;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StartGame();
+        }
+
+
+        public void StartGame()
+        {
+            player[] players = new player[PlayerNum];
+            int BoardSize = 7;
+            int TotalGrid = BoardSize * BoardSize;
+            int GapSize = 5;
+            int ButtonSize = 35;
+            ship[] AllShip = new ship[5];
+            Button[] GameButton = new Button[TotalGrid * 2];
+            int index = 0;
+
+            //create player struct to set the game
+            //create ship struct, for all player
+            for (int i = 0; i < PlayerNum; i++)
+            {
+                players[i].board = new char[2, BoardSize, BoardSize];
+                players[i].TotalRemain = 5 + 4 + 3 + 3 + 2;
+                players[i].AllShip = new ship[5];
+                for (int j = 0; j < 5; j++)
+                {
+                    players[i].AllShip[j].number = j;
+                    players[i].AllShip[j].remain = length[j];
+                    players[i].AllShip[j].name = ShipName[j];
+                }
+            }
+
+            //create game button
+            for (int i = 0; i < TotalGrid * 2; i++)
+            {
+                GameButton[i] = new Button();
+                GameButton[i].Size = new Size(ButtonSize, ButtonSize);
+                GameButton[i].Text = i.ToString();
+                GameButton[i].Name = i.ToString();
+                GameButton[i].Click += new EventHandler(ButtonClick);
+                this.Controls.Add(GameButton[i]);
+            }
+
+            //locate the button position
+            for (int i = 0; i < BoardSize; i++)
+            {
+                for (int j = 0; j < BoardSize; j++)
+                {
+                    GameButton[index].Location = new Point(120 + j * (ButtonSize + GapSize), 60 + i * (ButtonSize + GapSize));
+                    GameButton[index + 49].Location = new Point(460 + j * (ButtonSize + GapSize), 60 + i * (ButtonSize + GapSize));
+                    index++;
+                }
+
+            }
+
+            //location newlocate;
+            //newlocate.x_axis = 0;
+            //newlocate.y_axis = 1;
+            //index = 10;
+            //          SetShip(players[0], 0, newlocate, -1, GameButton[10]);
+            LogText.Text = returnValue.ToString();
+            for (int i = 0; i < 5; i++)
+            {
+                LogText.Text = "Please choose location to locate your " + ShipName[i];
+
+
+            }
+
+        }
+
+        //click button event
+        private void ButtonClick(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+//            LogText.Text = btn.Name + " clicked";
+            returnValue = Convert.ToInt32(btn.Name);
+        }
+
+        //enter a location, translate to index number
+        //(0,0) will be 0, (0,1) will be 7
+        private int Translate(location location)
+        {
+            int index;
+            index = location.x_axis + location.y_axis * 7;
+            return index;
+        }
+
+
+        //setting ships for player
+
+        private void SetShip(player player, int ship, location location, int direction, Button GameButton)
+        {
+            //direction variable 0 = horizontal, 1 = vertical
+            int DierctionTemp;
+            int index;
+            char symbol = ShipName[ship][0];
+            index = Translate(location);
+
+            if (direction == -1)
+            {
+                if (Horizontal.Checked == true)
+                {
+                    DierctionTemp = 0;
+                }
+                else
+                {
+                    DierctionTemp = 1;
+                }
+            }
+            else
+            {
+                DierctionTemp = direction;
+            }
+            player.board[0, location.x_axis, location.y_axis] = symbol;
+            player.AllShip[ship].location.x_axis = location.x_axis;
+            player.AllShip[ship].location.y_axis = location.y_axis;
+            player.AllShip[ship].direction = DierctionTemp;
+            GameButton.Text = symbol.ToString();
+            LogText.Text = "setting success \n";
+        }
+
+        //private void Shot()
+        //{
+
+
+        //}
+
+
+        private void ExitBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
